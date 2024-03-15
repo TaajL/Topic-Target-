cbc959ea4d87aa3f0b0ec2f52dd54cf8353825bc
FonKnp <fonnfonk@gmail.com>
2024-03-14 21:29:33 -0400

    add modal, button event listener, and basic CSS for modal

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+let modalBox = document.getElementById('modal');
+let startSearchBtn = document.getElementById('start-search');
+
+startSearchBtn.onclick = function() {
+  modalBox.style.display = 'none';
+}
