95a45f5c18bd7769c522f9029f0b98703cd7a1c4
Malcom X <Taajlogan@gmail.com>
2024-03-18 20:27:57 -0400

    added search button and event listener and created search funtion and started wiki fetch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,14 @@
 let modalBox = document.getElementById('modal');
 let startSearchBtn = document.getElementById('start-search');
-
+const searchBtn = document.getElementById('search-button');
 startSearchBtn.onclick = function() {
   modalBox.style.display = 'none';
 }
+
+
+
+
+searchBtn.addEventListener('click', search);
+function search(){ let wikiSearch = document.getElementById('userInput').value
+fetch(`https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${wikiSearch}&utf8=&format=json`)}
+
