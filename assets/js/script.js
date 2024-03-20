77f067e645e2f23f442e5bec13f27df22f424467
Katie Bug <katierosealford@gmail.com>
2024-03-20 18:24:07 -0400

    added delete button event listener and function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 let modalBox = document.getElementById('modal');
 let startSearchBtn = document.getElementById('start-search');
 const searchBtn = document.getElementById('search-button');
+const DeleteBtn = document.getElementById('delete-button');
+
 startSearchBtn.onclick = function() {
   modalBox.style.display = 'none';
 }
@@ -12,3 +14,9 @@ searchBtn.addEventListener('click', search);
 function search(){ let wikiSearch = document.getElementById('userInput').value
 fetch(`https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${wikiSearch}&utf8=&format=json`)}
 
+
+DeleteBtn.addEventListener('click', deleteAll);
+
+function deleteAll(){
+  localStorage.clear();
+}
