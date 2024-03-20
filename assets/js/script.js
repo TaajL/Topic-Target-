ff4d49c50c57306011187dcfb0a9ffd9e85117a8
Malcom X <Taajlogan@gmail.com>
2024-03-20 19:18:49 -0400

    finished wiki function

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,22 @@ startSearchBtn.onclick = function() {
 
 
 searchBtn.addEventListener('click', search);
-function search(){ let wikiSearch = document.getElementById('userInput').value
-fetch(`https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${wikiSearch}&utf8=&format=json`)}
+function search() { 
+  let wikiSearch = document.getElementById('userInput').value;
+
+fetch(`https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${wikiSearch}&utf8=&format=json`)
+  .then(response => response.json())
+  .then(data => {
+    const searchResults = data.query.search;
+    let resultList = '';
+
+    searchResults.forEach(item =>{
+      resultList += `<li><a href="https://en.wikipedia.org/?curid=${item.pageid}" target="_blank`
+    })
+  })
+
+
+
+
+}
 
