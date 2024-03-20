05294099b29279d8d269f061d49fdaa6e3f3369d
Malcom X <Taajlogan@gmail.com>
2024-03-20 19:36:44 -0400

    added search function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,21 @@
 let modalBox = document.getElementById('modal');
 let startSearchBtn = document.getElementById('start-search');
 const searchBtn = document.getElementById('search-button');
+let searchInput = document.getElementById('userInput').value;
+
 startSearchBtn.onclick = function() {
   modalBox.style.display = 'none';
 }
 
-
+function search() {
+  fetchYoutubeApis();
+  fetchWikiapi();
+}
 
 
 searchBtn.addEventListener('click', search);
-function search() { 
-  let wikiSearch = document.getElementById('userInput').value;
-
-fetch(`https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${wikiSearch}&utf8=&format=json`)
+function fetchWikiapi() { 
+fetch(`https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${searchInput}&utf8=&format=json`)
   .then(response => response.json())
   .then(data => {
     
